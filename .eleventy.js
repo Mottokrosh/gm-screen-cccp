c2cb6c2ee9ae853c4b0ac586d9d79f0cbcdaafbf
Frank Reding <frank.reding@gmail.com>
2021-03-19 21:14:53 +0100

    Tidy up, remove unused bits, add graphic

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("styles.css");
   eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addPassthroughCopy("fonts");
 
   return {
     dir: {
